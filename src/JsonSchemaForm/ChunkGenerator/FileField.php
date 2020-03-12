ad51b93750ad953ae4a90a04875ca600a24d380d
Daniel <dcherrington@enable.services>
2020-03-12 16:58:12 +0000

    Added file field

---
+++ src/JsonSchemaForm/ChunkGenerator/FileField.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace JsonSchemaForm\ChunkGenerator;
+
+class StringField extends \JsonSchemaForm\ChunkGenerator {
+	public function render($options = array()) {
+		if (empty($this->schema->enum)) {
+			$inputType = (isset($this->schema->inputType) ? $this->schema->inputType : 'input');
+			$options['type'] = (isset($this->schema->format) ? $this->schema->format : 'text');
+           		$options['required'] = isset($this->schema->required) ? $this->schema->required : 0;
+			return $this->_render('chunk/' . $inputType . '.twig', $options);
+		}
+
+		$options['options'] = array();
+		$enumTitles = (isset($this->schema->enumTitles) ? (object) $this->schema->enumTitles : new \StdClass());
+		foreach($this->schema->enum as $enumValue) {
+			$options['options'][] = array(
+				'id' =>  $this->getDomCompatible(implode(array_merge($this->path, array($enumValue)),  '-')),
+				'label' => (isset($enumTitles->$enumValue) ? $enumTitles->$enumValue: $enumValue),
+				'value' => $enumValue
+			);
+		}
+
+
+		$inputType = (isset($this->schema->inputType) ? $this->schema->inputType : 'select');
+		return $this->_render('chunk/' . $inputType . '.twig', $options);
+	}
+}
